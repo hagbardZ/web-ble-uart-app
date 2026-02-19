4208c00af1187487886be3bfef474f0ccbfdf624
Beha_S <stephan.beha@wahlgmbh.com>
2026-02-19 08:04:15 +0100

    changed graphics

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
     define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-    __APP_GIT_REPO_PATH__: JSON.stringify("https://github.com/SiliconLabsSoftware/web-bluetooth-spp-application"),
+    __APP_GIT_REPO_PATH__: JSON.stringify("https://github.com/hagbardZ/web-ble-uart-app"),
     __APP_SPP_BLE_SERVICE__: JSON.stringify("49535343-fe7d-4ae5-8fa9-9fafd205e455"),
     __APP_SPP_BLE_CHARACTERISTIC__: JSON.stringify("49535343-1e4d-4bd9-ba61-23c647249616"),
   },
@@ -15,5 +15,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
-  base: "/web-bluetooth-spp-application/"
+  base: "/web-ble-uart-app"
 });
